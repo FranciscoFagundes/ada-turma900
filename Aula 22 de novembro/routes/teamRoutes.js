d93207787f6b81c06f6f28159af35524adda4266
Francisco <francisco.fagundes@bluepanda.pt>
2022-11-22 20:22:33 +0000

    Update aula 22 de novembro

--- Aula 22 de novembro/routes/teamRoutes.js
+++ Aula 22 de novembro/routes/teamRoutes.js
@@ -3,6 +3,17 @@ const { Router } = require('express');
 const router = Router();
 
 
+const teams = [
+    {
+        "id": "01",
+        "name": "Brasil",
+        "coach": "Tite",
+        "championships": "5",
+        "mascot": "canário",
+        "worldcups": "22",
+        "flag": "https://imagepng.org/bandeira-do-brasil/bandeira-do-brasil/"
+    }
+];
 router.get('/', (req,res) => {
     res.send("Página raiz dos times");
 });
